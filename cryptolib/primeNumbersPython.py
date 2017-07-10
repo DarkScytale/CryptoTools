558400ba0221a58ee0e05711ba05780e113731ea
sebastien.vinot@prevention-informatique.fr <sebastien.vinot@prevention-informatique.fr>
2017-07-10 19:31:59 +0200

    Optimization of prime function

--- cryptolib/primeNumbersPython.py
+++ cryptolib/primeNumbersPython.py
@@ -11,6 +11,41 @@ def isPrime(n):
         d += 1
     return True
 
+def isPrimeOpt1(n):
+    """"This function test if the number n is a prime number.
+    Optimization 1: Do not test all even numbers.
+    """
+    if 0 == math.fmod(n, 2):
+        return False
+    maxTest = math.ceil( math.sqrt(n) + 1)
+    d = 3
+    while d <= maxTest:
+        if 0 == math.fmod(n, d):
+            return False
+        d += 2
+    return True
+
+def isPrimeOpt2(n):
+    """This function test if the number n is a prime number.
+    Optimization 1: Do not test all even numbers.
+    Optimization 2: Do not test all numbers ending with 5.
+    """
+    if 0 == math.fmod(n, 2):
+        return False
+    if 0 == math.fmod(n, 5):
+        return False
+
+    maxTest = math.ceil( math.sqrt(n) + 1)
+    d = 3
+    while d <= maxTest:
+        if 5 == d & 5:
+            d += 2
+            continue
+        if 0 == math.fmod(n, d):
+            return False
+        d += 2
+    return True
+
 def listPrime(n):
     """Returns a list for prime numbers included in [2, n].
     """
--- tests/speed/testPrimeSpeed.py
+++ tests/speed/testPrimeSpeed.py
@@ -1,16 +1,24 @@
-from cryptolib import primeNumbersPython as pnp
-from cryptolib import primeNumbersNumSci as pnns
+import sys, os, importlib
+
+#from cryptolib import primeNumbersPython as pnp
+#from cryptolib import primeNumbersNumSci as pnns
 
 import timeit
 
 nbTest = 10
+
 numberToTest = 100000000000031
 
 print("Lancement du test de vitesse des fonctions en pure Python ...")
 
-tPython1 = timeit.timeit('import cryptolib.primeNumbersPython as pnp; pnp.isPrime({0})'.format(numberToTest), number=nbTest)
+tPython1 = timeit.timeit('sys.path.append("./../"); import cryptolib.primeNumbersPython as pnp; pnp.isPrime({0})'.format(numberToTest), number=nbTest)
+print("Fonction de base : {0} sec".format(tPython1/nbTest))
 
-print("Fonction de base : {0}".format(tPython1))
+tPython2 = timeit.timeit('sys.path.append("./../"); import cryptolib.primeNumbersPython as pnp; pnp.isPrimeOpt1({0})'.format(numberToTest), number=nbTest)
+print("Fonction optimise 1 : {0} sec".format(tPython2/nbTest))
+
+tPython3 = timeit.timeit('sys.path.append("./../"); import cryptolib.primeNumbersPython as pnp; pnp.isPrimeOpt2({0})'.format(numberToTest), number=nbTest)
+print("Fonction optimise 2 : {0} sec".format(tPython3/nbTest))
 
 print("FIN")
 
